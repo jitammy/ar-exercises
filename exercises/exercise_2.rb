5ab4620972f2b97b27e85624f373af6d9df2d0bc
jitammy <jitammy@gmail.com>
2019-06-11 17:37:25 +0000

    step2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store1.name = "Waterloo"
+puts @store1.name
+
+@store2 = Store.find_by(id: 2)
+
+puts @store2.name

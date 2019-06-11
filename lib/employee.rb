670346c23b34388ffe072687c9a601e1d6e2c51d
jitammy <jitammy@gmail.com>
2019-06-11 19:49:02 +0000

    step 6

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,11 @@
 class Employee < ActiveRecord::Base
+  belongs_to :store
 
+  @store1.employees.create(first_name: "Arya", last_name: "Stark", hourly_rate: 60)
+  @store1.employees.create(first_name: "John", last_name: "Snow", hourly_rate: 30)
+  @store1.employees.create(first_name: "Sansa", last_name: "Stark", hourly_rate: 20)
+  @store2.employees.create(first_name: "Cersei", last_name: "Lannister", hourly_rate: 60)
+  @store2.employees.create(first_name: "Tyrion", last_name: "Lannister", hourly_rate: 30)
+  @store3.employees.create(first_name: "Jamie", last_name: "Lannister", hourly_rate: 20)
+  
 end

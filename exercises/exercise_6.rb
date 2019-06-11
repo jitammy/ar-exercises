9a01204060510db1eb429c905d6b83549a23e84f
jitammy <jitammy@gmail.com>
2019-06-11 20:31:23 +0000

    step 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,12 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Arya", last_name: "Stark", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Snow", hourly_rate: 40)
+@store1.employees.create(first_name: "Sansa", last_name: "Stark", hourly_rate: 50)
+@store2.employees.create(first_name: "Cersei", last_name: "Lannister", hourly_rate: 60)
+@store2.employees.create(first_name: "Tyrion", last_name: "Lannister", hourly_rate: 40)
+@store2.employees.create(first_name: "Jamie", last_name: "Lannister", hourly_rate: 50)
+
+pp @store1.employees.inspect
+
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,11 +1,8 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, presence: true, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store_id, presence: true
 
-  @store1.employees.create(first_name: "Arya", last_name: "Stark", hourly_rate: 60)
-  @store1.employees.create(first_name: "John", last_name: "Snow", hourly_rate: 30)
-  @store1.employees.create(first_name: "Sansa", last_name: "Stark", hourly_rate: 20)
-  @store2.employees.create(first_name: "Cersei", last_name: "Lannister", hourly_rate: 60)
-  @store2.employees.create(first_name: "Tyrion", last_name: "Lannister", hourly_rate: 30)
-  @store3.employees.create(first_name: "Jamie", last_name: "Lannister", hourly_rate: 20)
-  
 end

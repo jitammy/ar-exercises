d14f01944743c5fbd3de927aed91bde4facebcc0
jitammy <jitammy@gmail.com>
2019-06-11 18:26:56 +0000

    step 5

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,23 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true )
+Store.create(name: "Whislter", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false )
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true )
+
+# puts Store.class
+
+@mens_stores = Store.where(mens_apparel:true)
+
+# pp @mens_stores.inspect
+
+
+@mens_stores.each do |i|
+  puts "#{i.name} has annual revenue #{i.annual_revenue}"
+end
+
+
+
+
+
+
